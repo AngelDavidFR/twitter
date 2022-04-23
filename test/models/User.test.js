c91a59380c3fca9568f73ef2b737de27b0c17678
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-04-23 18:40:58 -0500

    Prueba de unidad

---
+++ test/models/User.test.js
@@ -0,0 +1,19 @@
+const User=require('./../../app/models/User')
+describe("Unit Test for User class",()=>{
+test('Create an User object',()=>{
+
+const user=new User(1,"AngelD_FloresR","Angel","Bio","dateCreated","lastUpdated")
+
+expect(user.id).toBe(1)
+expect(user.username).toBe("AngelD_FloresR")
+expect(user.name).toBe("dateCreated")
+expect(user.bio).toBe("Bio")
+expect(user.dateCreated).toBe("dateCreated")
+expect(user.lastUpdated).toBe("lastUpdated")
+
+
+
+});
+
+
+})
