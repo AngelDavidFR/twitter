e3bc05336854bf511820451545305d920fea099a
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-04-23 18:56:55 -0500

    Agregar constructor

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -6,7 +6,7 @@ const user=new User(1,"AngelD_FloresR","Angel","Bio","dateCreated","lastUpdated"
 
 expect(user.id).toBe(1)
 expect(user.username).toBe("AngelD_FloresR")
-expect(user.name).toBe("dateCreated")
+expect(user.name).toBe("Angel")
 expect(user.bio).toBe("Bio")
 expect(user.dateCreated).toBe("dateCreated")
 expect(user.lastUpdated).toBe("lastUpdated")
