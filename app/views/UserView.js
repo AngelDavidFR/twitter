c2c8a14eae4dbb92bee998216ff3fff305ac2e78
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-04-24 17:39:01 -0500

    Prueba de unidad requerimiento 2

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -2,13 +2,24 @@ const User= require('./../services/UserService')
 class UserView{
 static createUser(payload){
 let result={error:""}
+// const anyNull=true
 if(payload!=null){
+if(hasNull(payload)!=true){
 
+}else{
+    result.error="necesitan tener un valor válido"
+    return result
+}
 }else{
     result.error="payload no existe"
     return result
-}
-}
-}
+}}}
+function hasNull(target) {
+    for (var member in target) {
+      if (target[member] == null) return true;
+    }
+    return false;
+  }
+  
 module.exports=UserView
 
