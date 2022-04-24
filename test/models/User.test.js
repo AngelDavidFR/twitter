7eb97b367624de4018fd5a8c69d021e09d3a6540
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-04-23 19:00:03 -0500

    Prueba valor undefined

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,13 +2,13 @@ const User=require('./../../app/models/User')
 describe("Unit Test for User class",()=>{
 test('Create an User object',()=>{
 
-const user=new User(1,"AngelD_FloresR","Angel","Bio","dateCreated","lastUpdated")
+const user=new User(1,"AngelD_FloresR","Angel","Bio")
 
 expect(user.id).toBe(1)
 expect(user.username).toBe("AngelD_FloresR")
 expect(user.name).toBe("Angel")
 expect(user.bio).toBe("Bio")
-expect(user.dateCreated).toBe("dateCreated")
+expect(user.dateCreated).not.toBeUndefined()
 expect(user.lastUpdated).toBe("lastUpdated")
 
 
