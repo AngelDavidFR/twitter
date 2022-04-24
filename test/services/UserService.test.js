567ae7d6efb094ccbc1ecdc88539c2c3343be26a
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-04-24 10:53:16 -0500

    Prueba de Unidad data list

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,11 +1,19 @@
-const UserServices=require('./../../app/services/UserService')
+const UserService=require('./../../app/services/UserService')
 describe("Test for UserService",()=>{
 
     test("1. Create a new user using the UserService",()=>{
-        const user= UserServices.create(1,"AngelD_FloresR","Angel")
+        const user= UserService.create(1,"AngelD_FloresR","Angel")
         expect(user.username).toBe("AngelD_FloresR")
         expect(user.name).toBe("Angel")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
+    })
+    test("2. get all user data in a list",()=>{
+        const user=UserService.create(1,"AngelD_FloresR","Angel")
+        const userInfoInList=UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[0]).toBe("AngelD_FloresR")
+        expect(userInfoInList[0]).toBe("Angel")
+        expect(userInfoInList[0]).toBe("Sin bio")       
     })
 })
